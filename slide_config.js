9b914da088be63ffbf5c3067629ff96dffecdd08
Victor Duarte da Silva <ds.victor94@gmail.com>
2016-03-04 20:39:01 -0300

    #

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Minicurso Python',
+    //subtitle: 'Subtitle Goes Here',
+    eventInfo: {
+      title: 'SAICC',
+      date: '11/2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/python-icon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Victor Duarte da Silva<br>',
+    company: 'IFRS - Campus Rio Grande',
+    gplus: 'https://plus.google.com/100618278366539880916/',
+    twitter: '@dsvictor94',
+    //www: '//ds.victor94@gmail.com',
+    github: 'https://github.com/dsvictor94'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
